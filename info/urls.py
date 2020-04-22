f6211f9f2b1c9e2b9739adcdbeb7f641734071ce
pallav <ayush.pallav@innovaccer.com>
2020-04-23 02:46:14 +0530

    added bulk_retrieve api

--- info/urls.py
+++ info/urls.py
@@ -2,12 +2,13 @@ from django.urls import path, include
 from django.conf.urls import url
 from rest_framework import routers
 
-from info.views import UserDetailView
+from info.views import UserDetailView, UserDetailBulk
 
 router = routers.DefaultRouter()
 router.register(r'user', UserDetailView)
 
 
 urlpatterns=[
+	path('/bulk_retrieve', UserDetailBulk.as_view(), name='bulk'),
 	url('/', include(router.urls)),
 ]
